259c964e3a0e51956e991ffa150b1292b927c38c
Paulo Veras <54992276+pverasp@users.noreply.github.com>
2023-10-05 19:03:26 -0300

    Finalizando exercicio 3

--- javascript/modulo-d/exercicios-p1-p2-p3/ex015/script.js
+++ javascript/modulo-d/exercicios-p1-p2-p3/ex015/script.js
@@ -1,22 +1,49 @@
 function verificar() {
     var data = new Date()
     var ano = data.getFullYear()
+    var mes = data.getMonth()
+    
     var fano = window.document.getElementById('itxtano')
     var res = window.document.querySelector('div#resultado')
+
+    
+
     if (fano.value.length == 0 || Number(fano.value) > ano) {
         window.alert('Verifique os dados e tente novamente!')
-
     } else {
-        window.alert(`${ano}`)
+        var img = window.document.createElement('img')
+        img.setAttribute('id','foto')
+
         var fsex = window.document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
-        // res.innerHTML = `Idade calculada: ${idade}`
-        if (fsex[1].checked) {
-            res.innerHTML += `<br>Gênero: Feminino`
-        } else {
-            res.innerHTML += `<br>Gênero: Masculino`
-        }
 
+        var genero = ''
+
+        if (fsex[1].checked) {
+            genero = 'Mulher'
+            if (idade < 10){
+                img.setAttribute('src','imagens/bebe_f_circle.png')
+            } else if (idade < 18) {
+                img.setAttribute('src','imagens/jovem_f_circle.png')
+            } else if (idade < 50) {
+                img.setAttribute('src','imagens/adulto_f_circle.png')
+            } else {
+                img.setAttribute('src','imagens/idoso_f_circle.png')
+            } 
+        } else {
+            genero = 'Homen'
+            if (idade < 10){
+                img.setAttribute('src','imagens/bebe_m_circle.png')
+            } else if (idade < 18) {
+                img.setAttribute('src','imagens/jovem_m_circle.png')
+            } else if (idade < 50) {
+                img.setAttribute('src','imagens/adulto_m_circle.png')
+            } else {
+                img.setAttribute('src','imagens/idoso_m_circle.png')
+            } 
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com idade de ${idade} anos`
+        res.appendChild(img)
     }
-    
 }
