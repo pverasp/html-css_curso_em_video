5b2cba1d3ab15501b0de66c841de7e17250e8642
Paulo Veras <54992276+pverasp@users.noreply.github.com>
2023-10-13 06:56:42 -0300

    Atualizado nome da variavel
    de numerro para numNumero

--- javascript/modulo-e/exercicios-p4-p5-p6/ex017/modelo/script.js
+++ javascript/modulo-e/exercicios-p4-p5-p6/ex017/modelo/script.js
@@ -1,17 +1,17 @@
 // alert('Olá gafanhotos!')
 function tabuada() {
-    var numero = document.getElementById('ItxtNumero')
+    var txtNumero = document.getElementById('ItxtNumero')
     var txtTabuada = document.getElementById('ItxtTabuada') // select ItxtTabuada
     // window.alert(p_res[9].value)
     // window.alert(numero.value)
-    if (numero.value == '') {
+    if (txtNumero.value == '') {
         window.alert('Informe um número!')
     } else {
-        var numero2 = Number(numero.value)
+        var numNumero = Number(txtNumero.value)
         var res_tab = 0
         for (var inicio = 0;inicio <= 10; inicio++) {
-            res_tab = numero2 * inicio
-            txtTabuada[inicio].textContent = `${numero2} * ${inicio} = ${res_tab}`
+            res_tab = numNumero * inicio
+            txtTabuada[inicio].textContent = `${numNumero} * ${inicio} = ${res_tab}`
         }
     }
 
