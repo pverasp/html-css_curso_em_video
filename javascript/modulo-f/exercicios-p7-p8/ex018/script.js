a5c61a7531e145cd361264b43eb2361e5e1a376d
Paulo Veras <54992276+pverasp@users.noreply.github.com>
2023-10-15 07:53:41 -0300

    Update script.js

--- javascript/modulo-f/exercicios-p7-p8/ex018/script.js
+++ javascript/modulo-f/exercicios-p7-p8/ex018/script.js
@@ -28,6 +28,8 @@ function adicionar() {
     }
 }
 function finalizar() {
+    var resultado = window.document.getElementById('resposta')
+    resultado.innerHTML=''
     if (arrayNumeros.length==0) {
         window.alert('Adicione os números entre 1 e 100!')
 
@@ -38,19 +40,56 @@ function finalizar() {
         resultado.appendChild(paragrafo)
     
         paragrafo = document.createElement('p')
-        paragrafo.innerHTML = `O maior valor informado foi ...`
+        paragrafo.innerHTML = `O maior valor informado foi ${funcMaior()}`
         resultado.appendChild(paragrafo)
     
         paragrafo = document.createElement('p')
-        paragrafo.innerHTML = `O menor valor informado foi ...`
+        paragrafo.innerHTML = `O menor valor informado foi ${funcMenor()}`
+        resultado.appendChild(paragrafo)
+
+        // Ordenar o array. E com esta função podemos fazer uma comparação entre os valores e ordenar os elementos de um array
+        let numbersAsc = arrayNumeros.sort(function(a, b) {
+            return a - b;
+        });
+
+        paragrafo = document.createElement('p')
+        paragrafo.innerHTML = `2 - O maior valor informado foi ${numbersAsc[numbersAsc.length-1]}`
+        resultado.appendChild(paragrafo)
+
+        paragrafo = document.createElement('p')
+        paragrafo.innerHTML = `2 - O menor valor informado foi ${numbersAsc[0]}`
+        // paragrafo.innerHTML = numbersAsc
+        resultado.appendChild(paragrafo)
+    
+        let somaValores = funcSoma()
+        paragrafo = document.createElement('p')
+        paragrafo.innerHTML = `Somando todos os valores temos ${somaValores}`
         resultado.appendChild(paragrafo)
     
         paragrafo = document.createElement('p')
-        paragrafo.innerHTML = `Somando todos os valores temos `
-        resultado.appendChild(paragrafo)
-    
-        paragrafo = document.createElement('p')
-        paragrafo.innerHTML = `A média dos valores digitados é ...`
+        paragrafo.innerHTML = `A média dos valores digitados é ${somaValores/arrayNumeros.length}`
         resultado.appendChild(paragrafo)            
     }
 }
+function funcMaior() {
+    var numeroMaior = arrayNumeros[0]
+    for (let pos in arrayNumeros) {
+        numeroMaior = (numeroMaior<arrayNumeros[pos]?arrayNumeros[pos]:numeroMaior)
+    }
+    return numeroMaior
+}
+function funcMenor() {
+    var numeroMenor = arrayNumeros[0]
+    for (let pos in arrayNumeros) {
+        numeroMenor = (numeroMenor>arrayNumeros[pos]?arrayNumeros[pos]:numeroMenor)
+    }
+    return numeroMenor
+}
+function funcSoma() {
+    var vSoma = 0
+    for (let pos in arrayNumeros) {
+        vSoma += arrayNumeros[pos]
+    }
+    return vSoma
+}
+
